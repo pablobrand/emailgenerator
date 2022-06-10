bef235e3d28b309f489ed5d154c3db6107fffef1
pablobrand <pablo@zenledger.io>
2022-06-10 08:33:48 -0700

    working for just email generation, minus password

--- randomEmailgenerator.js
+++ randomEmailgenerator.js
@@ -3,39 +3,45 @@ const CryptoJS = require('crypto-js')
 const length = 12
 const chars = 'abcdefghijklmnopqrstuvwxyz1234567890'
 var string = ''
-const numberOfCredentials = 3
+const numberOfCredentials = 5
 
 const createRandEmailString = () => {
-  for (var ii = 0; ii < 15; ii++) {
+ 
+  for (var ii = 0; ii < 10; ii++) {
     string += chars[Math.floor(Math.random() * chars.length)]
   }
-  const emailValue = string
+  const emailValue = 'test'+string
   return emailValue
 }
 
 const createRanEmailCredentials = () => {
- 
+  const EmailCredentials = []
   for (var i = 0; i < numberOfCredentials; i++) {
-    let EmailCredentials = {
-      email: 'dfad@gmail.com',
-      password: 'dfsdfa',
-    }
-    
-    EmailCredentials.email = createRandEmailString()
-    const hash = CryptoJS.SHA256(string)
-    const hashToString = hash.toString()
-    EmailCredentials.password = hashToString.substring(0, length)
-    fs.writeFileSync(
-      'EmailCredentials.json',
-      JSON.stringify({
-        email: EmailCredentials.email + '@gmail.com',
-        password: EmailCredentials.password,
-      })
-    )
-    console.log('Email Address: ' + EmailCredentials.email + '@gmail.com')
-    //console.log('Password: ' + hash)
-    console.log('Trimed Password: ' + EmailCredentials.password)
+  EmailCredentials[i] = createRandEmailString()
+  string = ''
+  console.log('Email Address: ' + EmailCredentials[i] + '@gmail.com')
   }
+  // for (var i = 0; i < numberOfCredentials; i++) {
+  //   let EmailCredentials = {
+  //     email: 'dfad@gmail.com',
+  //     password: 'dfsdfa',
+  //   }
+    
+  //   EmailCredentials.email = createRandEmailString()
+  //   const hash = CryptoJS.SHA256(string)
+  //   const hashToString = hash.toString()
+  //   EmailCredentials.password = hashToString.substring(0, length)
+  //   fs.writeFileSync(
+  //     'EmailCredentials.json',
+  //     JSON.stringify({
+  //       email: EmailCredentials.email + '@gmail.com',
+  //       password: EmailCredentials.password,
+  //     })
+  //   )
+  //   console.log('Email Address: ' + EmailCredentials.email + '@gmail.com')
+  //   //console.log('Password: ' + hash)
+  //   console.log('Trimed Password: ' + EmailCredentials.password)
+  // }
   
 }
 
